1f5f4a2c1b639d2f9b62ddcd839379d406d26e23
noufalkdlr <noufalkakdlr@gmail.com>
2025-03-27 08:09:17 +0530

    state

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,27 +1,26 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 export default function Player({ name, symbol }) {
 
-    const [isEditing, setIsEditing] = useState(false)
+    const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing(true);
     }
 
-    let playerName = <span className="player-name" >{name}</span>
+    let plyerName = <span className="player-name" >{name}</span>
 
     if (isEditing) {
-        playerName = <input type="text" />
-
+        plyerName = <input type="text" />
     }
 
     return (
         <li>
             <span className="player" >
-                {playerName}
+                {plyerName}
                 <span className="player-symbol" >{symbol}</span>
             </span>
-            <button onClick={handleEditClick} >Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     )
 
